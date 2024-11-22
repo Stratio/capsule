a613057cc9ab4c0fd716311f094ce1fa5bc4715e
Unai Arrien <unaittxu@gmail.com>
2024-11-22 18:33:58 +0100

    [PLT-795] Bump Helm version during helm package to support chart OCI dependencies

--- bin/chart.sh
+++ bin/chart.sh
@@ -15,9 +15,10 @@ CHART_TEMP_DIR="target"
 mkdir -p "${CHART_TEMP_DIR}"
 
 cp -R charts/${NAME} "${CHART_TEMP_DIR}/${NAME}"
+# We need to bump Helm version during chart package to support chart OCI dependencies
+curl -sSLf https://raw.githubusercontent.com/helm/helm/v3.16.3/scripts/get-helm-3 | bash -s -- --version v3.16.3 --no-sudo
 helm package "${CHART_TEMP_DIR}/${NAME}" \
     --app-version=${VERSION} \
     --version=${VERSION} \
     --dependency-update \
     --destination=${CHART_TEMP_DIR}
-
