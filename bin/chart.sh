730639e16eefea46cdec62cd7c7780c38597976f
Unai Arrien <unaittxu@gmail.com>
2025-08-04 12:45:12 +0200

    Debug CICD helm chart

--- bin/chart.sh
+++ bin/chart.sh
@@ -9,15 +9,12 @@ NAME="capsule"
 VERSION="${1}"
 echo VERSION: ${VERSION}
 echo "Building HELM3 chart for ${NAME} ${VERSION} version"
-echo "HELM3 version $(helm version)"
 
 # Creating a new dir in the CI build environment
 CHART_TEMP_DIR="target"
 mkdir -p "${CHART_TEMP_DIR}"
 
 cp -R charts/${NAME} "${CHART_TEMP_DIR}/${NAME}"
-# We need to bump Helm version during chart package to support chart OCI dependencies
-curl -sSLf https://raw.githubusercontent.com/helm/helm/v3.16.3/scripts/get-helm-3 | bash -s -- --version v3.16.3 --no-sudo
 helm package "${CHART_TEMP_DIR}/${NAME}" \
     --app-version=${VERSION} \
     --version=${VERSION} \
